217df93927c8df18cf7793dc2ce9804011d78da6
Lu1zz <yu.lu@onekey.so>
2024-08-27 14:10:05 +0800

    fix: change derivation  path && update version
    Signed-off-by: Lu1zz <yu.lu@onekey.so>

--- core/src/apps/scdo/__init__.py
+++ core/src/apps/scdo/__init__.py
@@ -2,6 +2,6 @@ from apps.common.paths import PATTERN_BIP44
 
 CURVE = "secp256k1"
 SLIP44_ID = 541
-PATTERN = PATTERN_BIP44
+PATTERN = "m/44'/coin_type'/account'/0/0"
 PRIMARY_COLOR = 0xFFFFFF
 ICON = "A:/res/chain-scdo.png"
