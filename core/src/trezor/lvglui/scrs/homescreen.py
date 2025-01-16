64a9e66bc702d0e3f8270eca470d7cff82b56ba9
guowei0105 <guowei.zhou@onekey.so>
2025-01-16 17:06:34 +0800

    update  i18n

--- core/src/trezor/lvglui/scrs/homescreen.py
+++ core/src/trezor/lvglui/scrs/homescreen.py
@@ -2966,9 +2966,7 @@ class Autolock_and_ShutingDown(AnimScreen):
         gc.collect()
 
     def refresh_text(self):
-        self.title.set_text(
-            _(i18n_keys.ITEM__AUTO_LOCK) + " & " + _(i18n_keys.ITEM__SHUTDOWN)
-        )
+        self.title.set_text(_(i18n_keys.ITEM__AUTO_LOCK_AND_SHUTDOWN))
         self.auto_lock.label_left.set_text(_(i18n_keys.ITEM__AUTO_LOCK))
         self.auto_shutdown.label_left.set_text(_(i18n_keys.ITEM__SHUTDOWN))
 
