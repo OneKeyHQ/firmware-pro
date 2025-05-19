a29be4e39a760130ac5feff0a921977a5852a254
sunzhen <larriesun2007@gmail.com>
2025-05-19 10:13:57 +0800

    enable default theme

--- core/embed/lvgl/lv_conf.h
+++ core/embed/lvgl/lv_conf.h
@@ -682,7 +682,7 @@
  *==================*/
 
 /*A simple, impressive and very complete theme*/
-#define LV_USE_THEME_DEFAULT 0
+#define LV_USE_THEME_DEFAULT 1
 #if LV_USE_THEME_DEFAULT
 
     /*0: Light mode; 1: Dark mode*/
@@ -696,7 +696,7 @@
 #endif /*LV_USE_THEME_DEFAULT*/
 
 /*A very simple theme that is a good starting point for a custom theme*/
-#define LV_USE_THEME_SIMPLE 1
+#define LV_USE_THEME_SIMPLE 0
 
 /*A theme designed for monochrome displays*/
 #define LV_USE_THEME_MONO 0
