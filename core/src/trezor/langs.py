0ae53e7fb7527fc39179df6f6dd7f3ea088f2954
yangxf <yangxf0130@gmail.com>
2025-06-19 14:22:27 +0800

    emulator:using English only

--- core/src/trezor/langs.py
+++ core/src/trezor/langs.py
@@ -1,16 +1,16 @@
 # according to ISO_639-1 and ISO-3166 country codes
 langs = [
     ("en", "English"),
-    ("zh_cn", "简体中文"),
-    ("zh_hk", "繁體中文"),
-    ("ja", "日本語"),
-    ("ko", "한국어"),
-    ("fr", "Français"),
-    ("de", "Deutsch"),
-    ("ru", "Russian"),
-    ("es", "Spanish"),
-    ("it", "Italiano"),
-    ("pt_br", "Portuguese (Brazil)"),
+    # ("zh_cn", "简体中文"),
+    # ("zh_hk", "繁體中文"),
+    # ("ja", "日本語"),
+    # ("ko", "한국어"),
+    # ("fr", "Français"),
+    # ("de", "Deutsch"),
+    # ("ru", "Russian"),
+    # ("es", "Spanish"),
+    # ("it", "Italiano"),
+    # ("pt_br", "Portuguese (Brazil)"),
 ]
 
 langs_keys = [x[0] for x in langs]
